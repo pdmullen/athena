pdmullen/athena

commit 8a114bea4ab7b7cfebdbf3a8429d011f7a7ff777

Add two instance methods in DustParticles class.
Add accessor methods GetMassDensity() and GetVelocityField() to get the
mass density and velocity field of particles, respectively.

src/particles/dust_particles.cpp
@@ -138,6 +138,26 @@ DustParticles::~DustParticles() {
 }
 
 //--------------------------------------------------------------------------------------
+//! \fn AthenaArray<Real> DustParticles::GetVelocityField()
+//  \brief returns the particle velocity on the mesh.
+// Precondition: The particle properties on mesh must be assigned using the class method
+//   DustParticles::FindDensityOnMesh().
+
+AthenaArray<Real> DustParticles::GetVelocityField() const {
+  AthenaArray<Real> vel(3, ppm->nx3_, ppm->nx2_, ppm->nx1_);
+  for (int k = ppm->ks; k <= ppm->ke; ++k)
+    for (int j = ppm->ks; j <= ppm->ke; ++j)
+      for (int i = ppm->ks; i <= ppm->ke; ++i) {
+        Real rho = ppm->weight(k,j,i);
+        rho = (rho > 0.0) ? rho : 1.0;
+        vel(0,k,j,i) = ppm->meshaux(imom1,k,j,i) / rho;
+        vel(1,k,j,i) = ppm->meshaux(imom2,k,j,i) / rho;
+        vel(2,k,j,i) = ppm->meshaux(imom3,k,j,i) / rho;
+      }
+  return vel;
+}
+
+//--------------------------------------------------------------------------------------
 //! \fn Real DustParticles::NewBlockTimeStep();
 //  \brief returns the time step required by particles in the block.
 

src/outputs/outputs.cpp
@@ -624,7 +624,7 @@ void OutputType::LoadOutputData(MeshBlock *pmb) {
       pod = new OutputData;
       pod->type = "VECTORS";
       pod->name = "vp";
-      pod->data.InitWithShallowSlice(ppm->meshaux, 4, Particles::imom1, 3);
+      pod->data = pmb->ppar->GetVelocityField();
       AppendOutputDataNode(pod);
       num_vars_ += 3;
     }
@@ -635,15 +635,7 @@ void OutputType::LoadOutputData(MeshBlock *pmb) {
       pod = new OutputData;
       pod->type = "SCALARS";
       pod->name = "rhop";
-      pod->data.NewAthenaArray(ppm->weight.GetDim3(),
-                               ppm->weight.GetDim2(), ppm->weight.GetDim1());
-      const int is = ppm->is, js = ppm->js, ks = ppm->ks;
-      const int ie = ppm->ie, je = ppm->je, ke = ppm->ke;
-      const Real mass = DustParticles::GetOneParticleMass();
-      for (int k = ks; k <= ke; ++k)
-        for (int j = js; j <= je; ++j)
-          for (int i = is; i <= ie; ++i)
-            pod->data(k,j,i) = mass * ppm->weight(k,j,i);
+      pod->data = pmb->ppar->GetMassDensity();
       AppendOutputDataNode(pod);
       num_vars_++;
     }

src/particles/particles.hpp
@@ -224,6 +224,10 @@ friend class MeshBlock;
   // Destructor
   ~DustParticles();
 
+  // Accessors
+  AthenaArray<Real> GetMassDensity() const;
+  AthenaArray<Real> GetVelocityField() const;
+
   // Instance method
   Real NewBlockTimeStep();
 
@@ -285,6 +289,16 @@ inline Real DustParticles::GetOneParticleMass() {
 
 inline Real DustParticles::GetStoppingTime() {
   return taus0;
+}
+
+//--------------------------------------------------------------------------------------
+//! \fn AthenaArray<Real> DustParticles::GetMassDensity()
+//  \brief returns the mass density of particles on the mesh.
+// Precondition: The particle properties on mesh must be assigned using the class method
+//   DustParticles::FindDensityOnMesh().
+
+inline AthenaArray<Real> DustParticles::GetMassDensity() const {
+  return ppm->weight;
 }
 
 #endif  // PARTICLES_PARTICLES_HPP_

commit 65430d19f692152951c5103dd513e57f13f67cb8

Fix a bug introduced in commit 8a114bea4.

src/particles/dust_particles.cpp
@@ -162,9 +162,9 @@ DustParticles::~DustParticles() {
 AthenaArray<Real> DustParticles::GetVelocityField() const {
   AthenaArray<Real> vel(3, ppm->nx3_, ppm->nx2_, ppm->nx1_);
   for (int k = ppm->ks; k <= ppm->ke; ++k)
-    for (int j = ppm->ks; j <= ppm->ke; ++j)
-      for (int i = ppm->ks; i <= ppm->ke; ++i) {
-        Real rho = ppm->weight(k,j,i);
+    for (int j = ppm->js; j <= ppm->je; ++j)
+      for (int i = ppm->is; i <= ppm->ie; ++i) {
+        Real rho(ppm->weight(k,j,i));
         rho = (rho > 0.0) ? rho : 1.0;
         vel(0,k,j,i) = ppm->meshaux(imom1,k,j,i) / rho;
         vel(1,k,j,i) = ppm->meshaux(imom2,k,j,i) / rho;